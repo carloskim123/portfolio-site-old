e1543261ab99d499fe92b354e10b51767e527c4c
maninthehood <carloskirui154@gmail.com>
2023-08-10 10:48:21 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
 import './App.css';
-import Counter from './features/counter/Counter'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+
     </div>
   );
 }
