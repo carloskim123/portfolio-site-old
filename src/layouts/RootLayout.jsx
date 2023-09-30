79f702a69ea721c73e2cd0a3dc8e009c7c36d609
carloskirui <115598773+carloskim123@users.noreply.github.com>
2023-09-30 14:30:16 -0400

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,16 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Box, Flex, Text, Icon } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
-import { Suspense, useEffect, useState, useRef } from "react"; // Import useRef
+import { Suspense, useEffect, useState, useRef } from "react";
 import Loader from '../components/Loader';
 import { routes } from '../../data/db';
-import { Button, Icon } from '@chakra-ui/react';
+import "../RootLayout.css";
 
-export default function RootLayout() {
+
+
+
+
+const RootLayout = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [showMenu, setShowMenu] = useState(false);
   const navigate = useNavigate();
@@ -14,145 +18,136 @@ export default function RootLayout() {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 3000);
+    }, 5000);
   }, []);
-
-  const sidebarRef = useRef(null); // Create a ref for the sidebar
-
-  // Function to close the mobile menu when entering desktop view
-  const closeMobileMenu = () => {
-    if (window.innerWidth >= 768 && showMenu) {
-      setShowMenu(false);
-    }
-  };
 
   // Attach the event listener when the component mounts
   useEffect(() => {
     window.addEventListener('resize', closeMobileMenu);
-
-    // Add a click event listener to the document
-    // document.addEventListener('click', handleClickOutside);
-
     return () => {
       window.removeEventListener('resize', closeMobileMenu);
 
-      // Remove the click event listener when the component unmounts
-      // document.removeEventListener('click', handleClickOutside);
     };
   }, [showMenu]);
-
 
 
   const toggleMobileMenu = () => {
     setShowMenu(!showMenu);
   };
 
+  const closeMobileMenu = () => {
+    if (window.innerWidth >= 7 && showMenu) {
+      setShowMenu(false);
+    } else {
+      setShowMenu(false);
+    }
+  };
 
   return (
-    <div>
-      <Box py={2} px={4} borderBottom={"solid"} borderBottomColor={"black"} borderBottomWidth={"2px"} h={"50px"}>
-        <Flex gap={"2rem"} alignItems={"center"} fontSize={"18px"} >
-          <Text mr={"auto"} ml={"2%"}>
-            <Link to={"/"} >
-              <Box
-                mt={"10px"}
-                transition="transform 0.1s ease-in-out"
-                _hover={{
-                  borderBottom: "2px solid black",
-                  transform: "translateY(-5px)",
-                }}>Carlos.K</Box>
-            </Link>
-          </Text>
+    <Flex flexDirection="column" minH="100vh">
+      <Box py={2} px={4} borderBottom="2px solid black" h="50px">
+        <Flex justifyContent="space-between" alignItems="center" fontSize="18px">
+          <Link to="/">
+            <Text
+              mt="10px"
+              _hover={{
+                borderBottom: "2px solid black",
+                transform: "translateY(-5px)",
+              }}
+            >
+              Carlos.K
+            </Text>
+          </Link>
           <Box display={{ base: "block", md: "none" }} onClick={toggleMobileMenu}>
             {showMenu ? (
-              <Box
-                _hover={{ cursor: "pointer" }}
-                pl={"10%"}
-                onClick={() => setShowMenu(false)} // Close the sidebar when clicked
-              >
-                ✖️
-              </Box>
+              <Box onClick={closeMobileMenu} ></Box>
             ) : (
-              <svg fill="black" width="34px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 6h14v2H3zm0 5h14v2H3zm0 5h14v2H3z" />
-              </svg>
+              <Box onClick={toggleMobileMenu} _hover={{ cursor: "pointer" }} >
+                <svg fill="black" width="34px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M3 6h14v2H3zm0 5h14v2H3zm0 5h14v2H3z" />
+                </svg>
+              </Box>
+
             )}
           </Box>
-          <Flex display={{ base: "none", md: "flex" }} gap={"2rem"} >
+          <Flex display={{ base: "none", md: "flex" }} gap="2rem">
             {routes.map(route => (
-              <Box
-                key={route.path}
-                transition="transform 0.1s ease-in-out"
-                fontSize="20px"
-                _hover={{
-                  transform: "translateY(-2px)",
-                  borderBottom: "1px solid black",
-                }}
-              >
-                <Link to={route.path}>{route.pathname}</Link>
-              </Box>
+              <Link key={route.path} to={route.path}>
+                <Text
+                  fontSize="20px"
+                  _hover={{
+                    transform: "translateY(-2px)",
+                    borderBottom: "1px solid black",
+                  }}
+                >
+                  {route.pathname}
+                </Text>
+              </Link>
             ))}
           </Flex>
         </Flex>
       </Box>
-      {showMenu && (
+      <Flex flex="1">
         <Box
           position="fixed"
-          top="10"
-          left="0"
+          top="0"
+          left={showMenu ? "0" : "-100%"}
           zIndex="10"
           w="100%"
           h="100%"
-          onClick={() => setShowMenu(false)}
+          // className="sidebar-open"
           backdropFilter="blur(5px)"
-          background="rgba(0, 0, 0, 0.5)"
-          transition="opacity 1s, width 1s ease" // Add transitions
-          opacity={showMenu ? 1 : 0} // Control opacity based on showMenu state
+          background="rgba(0, 0, 0, 0.01)"
+          transition="left 0.3s ease-in-out"
+          onClick={closeMobileMenu}
         >
           <Box
+            className="sidebar"
             overflow="hidden"
-            h={showMenu ? "100vh" : "0"}
-            w={showMenu ? "50%" : "0"}
+            h="100vh"
+            w="80%"
             py={7}
-            px={4}
-            background={"#EDE0D4"}
-            borderBottom={"solid"}
-            borderBottomColor={"black"}
-            borderBottomWidth={"2px"}
+            px={6}
+            background="#EDE0D4"
+            borderBottom="2px solid black"
             display="flex"
             flexDir="column"
             gap=".5rem"
           >
             <Box
-              _hover={{ cursor: "pointer" }}
-              pl={"90%"}
-              onClick={() => setShowMenu(false)} // Close the sidebar when clicked
-            >
-              ✖️ 
-            </Box>
+              ml="auto"
+              onClick={closeMobileMenu}
+              cursor="pointer"
+              fontSize="20px"
+              _hover={{ transform: "scale(1.2)" }}
+            >X</Box>
             {routes.map(route => (
-              <Box
-                w={"100%"}
+              <Link
                 key={route.path}
-                transition="transform 0.1s ease-in-out"
-                fontSize="20px"
-                _hover={{ background: "gray.300", transform: "translateX(2px)" }}
+                to={route.path}
                 onClick={() => {
                   navigate(route.path);
-                  setShowMenu(false);
+                  closeMobileMenu();
                 }}
               >
-                {route.pathname}
-              </Box>
+                <Text
+                  fontSize="20px"
+                  _hover={{ transform: "translateX(2px)", cursor: "pointer" }}
+                >
+                  {route.pathname}
+                </Text>
+              </Link>
             ))}
-
           </Box>
         </Box>
-
-      )}
-      <Suspense fallback={<Loader />}>
-        {isLoading ? <Loader /> : <Outlet />}
-      </Suspense>
-    </div>
+        <Box flex="1" p="20px">
+          <Suspense fallback={<Loader />}>
+            {isLoading ? <Loader /> : <Outlet />}
+          </Suspense>
+        </Box>
+      </Flex>
+    </Flex>
   );
-}
+};
+
+export default RootLayout;
