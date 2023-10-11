c95c4e3f58ec96914c72a00a5c4bd2965b7c8bf3
carloskirui <115598773+carloskim123@users.noreply.github.com>
2023-10-11 16:58:53 -0400

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -80,7 +80,7 @@ const RootLayout = () => {
     setHasCheckedForUpdate(false);
     localStorage.setItem("hasCheckedForUpdate", false);
     localStorage.clear();
-  }, 5000)
+  }, 846000)
 
   // Determine whether to show the "Check for updates" button
   const showUpdateButton =
